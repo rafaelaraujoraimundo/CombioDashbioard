ce1bf3cd885e24ba7be0c94f02c4deb88f9a4787
Rafael Araujo <rafael.araujo2005@gmail.com>
2024-03-10 18:22:03 -0300

    INclusã de Graficos PO-UI na pagina fluig

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeDashboardComponent } from './pages/home-dashboard/home-dashboard.component';
 import { FluigComponent } from './pages/fluig/fluig.component';
+import { PoChartModule, PoGaugeModule, PoWidgetModule } from '@po-ui/ng-components';
 
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    PoGaugeModule,
+    PoChartModule,
+    PoWidgetModule
+    
   ],
   declarations: [HomeDashboardComponent, FluigComponent]
 })
--- src/app/modules/dashboard/pages/fluig/fluig.component.ts
+++ src/app/modules/dashboard/pages/fluig/fluig.component.ts
@@ -2,6 +2,7 @@ import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { DashboardService } from '../../services/dashboard.service';
 import { fluigServidores } from '../../interface/dashboard.interface';
 import { MenuService } from '../../../menu/services/menu.service';
+import { PoChartType, PoGaugeRanges } from '@po-ui/ng-components';
 
 @Component({
   selector: 'app-fluig',
@@ -12,6 +13,15 @@ export class FluigComponent implements OnInit {
 
   dataFluigServidores: fluigServidores[] = []
   public titleMenu: string = 'Dashboard Fluig';
+
+  line: PoChartType = PoChartType.Line;
+  column: PoChartType = PoChartType.Column;
+  donut: PoChartType = PoChartType.Donut;
+  bar: PoChartType = PoChartType.Bar;
+
+  widgetHeight: number = 400
+  urlBancoDados: String = '../../../../assets/Mysql_logo.png'
+
   constructor(private dashboardService: DashboardService, private menuService: MenuService, private cdr: ChangeDetectorRef) { }
 
   ngOnInit() {
@@ -26,5 +36,36 @@ export class FluigComponent implements OnInit {
     this.cdr.detectChanges();
   }
 
+  gauges = [
+    { value: 1200 , description: 'Memoria Usada 1200Mb' },
+  ];
 
+  pieUsing = [
+    { label: 'Angular', data: 100 },
+    { label: 'React', data: 10 }
+  ]
+
+  MemoriaUsadaRanges: Array<PoGaugeRanges> = [
+    { from: 0, to: 3999, label: 'Baixa Utilização', color: '#00b28e' },
+    { from: 4000, to: 4999, label: 'Utilização Média', color: '#ea9b3e' },
+    { from: 5000, to: 6000, label: 'Alta Utilização', color: '#c64840' }
+  ];
+
+  // Dados para o Gráfico de Linha
+  lineChartData = [
+    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Série A' },
+    { data: [28, 48, 40, 19, 86, 27, 90], label: 'Série B' }
+  ];
+
+  // Dados para o Gráfico de Barra
+  barChartData = [
+    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Série A' },
+    { data: [28, 48, 40, 19, 86, 27, 90], label: 'Série B' }
+  ];
+
+  // Categorias para os gráficos
+  chartCategories = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho'];
 }
+
+
+
